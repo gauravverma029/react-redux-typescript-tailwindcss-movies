1e059e1d36d3282b26a4aad605f645dca568d9c8
Gaurav Verma <gauravverma029@gmail.com>
2021-02-22 09:52:19 +0100

    fix container width about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="container relative">
+    <div className="">
       <h1 className="text-center text-xl md:text-4xl px-6 py-8 bg-blue-50 mt-4">About Us</h1>
     </div>
   );
